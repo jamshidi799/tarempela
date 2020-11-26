724e0049460405c32d4979b22304d0ae90e7fb90
jamshidi.m799 <jamshidi.m799@gmail.com>
2020-11-26 20:49:20 +0330

    add google tag manager

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -4,16 +4,39 @@ export const siteTitle = 'طارم پلا';
 
 export default function Header() {
   return (
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="طارم پلا برنج طارم درجه یک مازندران ارسال به تمام نقاط ایران" />
 
-      <meta property="og:title" content="طارم پلا برنج درجه یک مازندران" />
-      <meta property="og:site_name" content="tarempela" />
-      <meta property="og:url" content="http://tarempela.ir/" />
-      <meta property="og:description" content="طارم پلا برنج طارم درجه یک مازندران ارسال به تمام نقاط ایران" />
-      <meta property="og:type" content="business.business" />
-      <meta property="og:image" content="https://drive.google.com/file/d/1J8Uu703fgfUD_C2vx96kNeatYEBV2yw9/view?usp=sharing" />
-      <meta name="google-site-verification" content="ZtmciE-XG3LcOjDs5SjrppiQGqSPFZK446hs2uUJjPE" />
-    </Head>
+        <meta property="og:title" content="طارم پلا برنج درجه یک مازندران" />
+        <meta property="og:site_name" content="tarempela" />
+        <meta property="og:url" content="http://tarempela.ir/" />
+        <meta property="og:description" content="طارم پلا برنج طارم درجه یک مازندران ارسال به تمام نقاط ایران" />
+        <meta property="og:type" content="business.business" />
+        <meta
+          property="og:image"
+          content="https://drive.google.https://drive.google.com/uc?export=download&id=1J8Uu703fgfUD_C2vx96kNeatYEBV2yw9com/file/d/1J8Uu703fgfUD_C2vx96kNeatYEBV2yw9/view?usp=sharing"
+        />
+        <meta name="google-site-verification" content="ZtmciE-XG3LcOjDs5SjrppiQGqSPFZK446hs2uUJjPE" />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M7LWLS9');`,
+          }}
+        />
+      </Head>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7LWLS9"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      />
+    </>
   );
 }
