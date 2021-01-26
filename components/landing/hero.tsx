579e41bb6fa1cc49e7d53b05654747a44eb5c85e
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-01-26 11:40:56 +0330

    bug fix in slider

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -9,6 +9,10 @@ export default function Hero() {
   const [index, setIndex] = useState(0);
   const [slider, setSlider] = useState(0)
   useEffect(() => {
+    for (let i = 1; i < 4; i++) {
+      const slide = document.querySelector(`.slide-${i}`)
+      i === slider % 3 + 1 ? slide.setAttribute("style", "opacity: 1"): slide.setAttribute("style", "opacity: 0")
+    }
     const interval = setTimeout(() => setSlider(slider+1), 4000)
     return () => clearTimeout(interval)
   }, [slider]);
@@ -22,7 +26,6 @@ export default function Hero() {
     const area: { offsetWidth?: number; offsetHeight?: number } = heroRef.current;
     var docX = area.offsetWidth;
     var docY = area.offsetHeight;
-    console.log(docX);
     var moveX = ((e.pageX - docX / 2) / (docX / 2)) * -moveForce;
     var moveY = ((e.pageY - docY / 2) / (docY / 2)) * -moveForce;
     var rotateY = (e.pageX / docX) * rotateForce * 2 - rotateForce;
@@ -33,23 +36,13 @@ export default function Hero() {
     popup.style.transform = 'rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg)';
   };
 
-  const renderSlideImage = () => {
-    switch (slider % 3) {
-      case 1:
-        return <Image src="/images/parallax.webp" alt="طارم‌پلا" layout="fill" /> 
-      case 2:
-        return <Image src="/images/hero-1.webp" alt="طارم‌پلا" layout="fill" />
-      default:
-        return <Image src="/images/hero-2.webp" alt="طارم‌پلا" layout="fill" />
-    }
-  }
 
   return (
     <div className={style.hero} onMouseMove={onMove} ref={heroRef}>
       <div className={style.image_wrapper}>
-    
-        {renderSlideImage()}
-        
+      <Image className="slide-1" src="/images/parallax.webp" alt="طارم‌پلا" layout="fill" /> 
+      <Image className="slide-2" src="/images/hero-1.webp" alt="طارم‌پلا" layout="fill" />
+      <Image className="slide-3" src="/images/hero-2.webp" alt="طارم‌پلا" layout="fill" />
         </div>
       <div className={style.overlay}></div>
       <div className={style.popup} ref={popupRef}>
